2f9976a580119a20f15b05c5f81a2e95fdbe9ecf
ygs <ygs@cgcal.com>
2022-01-07 08:47:41 +0800

    done

--- S3Client.php
+++ S3Client.php
@@ -1,6 +1,12 @@
 <?php
 namespace yanggs07;
-
+/**
+ * @property string $version aws version, when use minio, fill it with `latest`
+ * @property string $region aws region, when use minio, fill it with `us-east-1`
+ * @property string $endpoint
+ * @property bool $use_path_style_endpoint
+ * @property array $credentials
+ */
 class S3Client extends \Aws\S3\S3Client implements \yii\base\Configurable
 {
 }
